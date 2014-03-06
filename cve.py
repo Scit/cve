96c7ed6db0eacfb3ccb148206db7d605cefc28eb
Andrey Terenkov <andrey.terenkov@gmail.com>
2014-03-06 22:14:38 +0400

    master - parallel implementation

--- cve.py
+++ cve.py
@@ -1,39 +1,61 @@
+#!/usr/bin/env python
 import sys
+import getopt
 import urllib2
 import urlparse
 import re
 
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
 
 
-target = None
-
-if '-f' in sys.argv:
-    if len(sys.argv) < 3:
-        print 'Use: cve.py [-f <outputfile>]'
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'f:')
+    except getopt.GetoptError:
+        print("Usage: %s [-f <filename>]" % sys.argv[0])
         sys.exit(2)
-    target = sys.argv[2]
 
-main_url = 'https://access.redhat.com'
-content = urllib2.urlopen(urlparse.urljoin(main_url, '/security/cve/'))
-soup = BeautifulSoup(content)
+    handle = sys.stdout
+    for opt, arg in opts:
+        if opt == '-f':
+            handle = open(arg, 'w')
 
-handle = open(target, 'w') if target else sys.stdout
+    cves_url = 'https://access.redhat.com'
+    content = urllib2.urlopen(urlparse.urljoin(cves_url, '/security/cve/'))
+    soup = BeautifulSoup(content)
+    cves = soup.find(id='cves_table').find('tbody').find_all('a', class_='internal')
 
-items = soup.find(id='cves_table').find('tbody').find_all('a', class_='internal')
-for item in items:
-    title = item.get_text()
+    cve_pairs = []
+    for cve in cves:
+        title = cve.get_text()
+        description_url = urlparse.urljoin(cves_url, cve['href'])
 
-    rhsa_site = urlparse.urljoin(main_url, item['href'])
-    rhsa_content = urllib2.urlopen(rhsa_site)
-    soup = BeautifulSoup(rhsa_content)
+        cve_pairs.append(
+            {'title': title,
+             'description_url': description_url}
+        )
+
+    pool = Pool(processes=20)
+    results = pool.map(get_cve_info, cve_pairs)
+
+    for result in results:
+        handle.write('%s %s\n' % result)
+
+    if handle is not sys.stdout:
+        handle.close()
+
+
+def get_cve_info(cve_pair):
+    content = urllib2.urlopen(cve_pair['description_url'], timeout=5).read()
+    soup = BeautifulSoup(content)
 
     try:
-        rhsa = soup.find('table', class_='docstable').find('td', text=re.compile('Red Hat Enterprise Linux version 6')).parent.find('a', class_='internal')['href']
+        errata = soup.find('table', class_='docstable').find('td', text=re.compile('Red Hat Enterprise Linux version 6')).parent.find('a', class_='internal')['href']
     except AttributeError:
-        rhsa = ''
+        errata = ''
+    return cve_pair['title'], errata
 
-    handle.write('%s %s\n' % (title, rhsa))
 
-if handle is not sys.stdout:
-    handle.close
+if __name__ == '__main__':
+    main()
